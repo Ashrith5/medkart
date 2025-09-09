b59eae0f255f6b4ff1196e877fc11536d314e37c
Ashrith5 <ashrithuppala5@gmail.com>
2025-09-09 15:15:12 +0530

    Update frontend dependencies and index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import 'antd/dist/reset.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/index'
-import store from './Stores/store';
-
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-  <Provider store = {store}>
-    <RouterProvider router={router}/>
-    </Provider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
